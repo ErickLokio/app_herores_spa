67c38dc4cfccc76a143cd59f01720c2a0d9c407c
Erick <eosoyi@outlook.com>
2023-02-06 08:22:22 -0600

    Se agrega codigo para mantener la info del usuario que hizo login

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -1,17 +1,40 @@
 import { useReducer } from "react";
+import { types } from "../types/types";
 import { AuthContext } from "./AuthContext";
 import { authReducer } from "./authReducer";
 
-const initialState = {
-    logged : false
+const init = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    return {
+        logged : !!user,
+        user : user
+    }
 }
 
 export const AuthProvider = ({ children }) => {
 
-    const [ state, dispach ] = useReducer( authReducer, initialState);
+    const [ authState, dispach ] = useReducer( authReducer, {}, init);
+
+    const login = ( name = '' ) => {
+
+        const user = { id: 'LKO', name };
+
+        const action = {
+            type: types.login,
+            payload: user
+        }
+
+        localStorage.setItem('user', JSON.stringify(user));
+
+        dispach(action);
+    }
 
     return (
-        <AuthContext.Provider value={{}}>
+        <AuthContext.Provider value={{
+            ...authState,
+            login: login
+        }}>
             { children }
         </AuthContext.Provider>
     );
